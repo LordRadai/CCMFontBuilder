d7e7cb52ea362c22ceab91a9baf3bd108394d34c
Radai <lordradai@null.net>
2024-03-06 12:41:21 +0100

    Update CCMFontBuilder.cpp

--- CCMFontBuilder/CCMFontBuilder/CCMFontBuilder.cpp
+++ CCMFontBuilder/CCMFontBuilder/CCMFontBuilder.cpp
@@ -50,6 +50,133 @@ bool FilterGlyph(WCHAR chr)
     return false;
 }
 
+bool WriteGlyphsToBitmapCharList(int* textureIdx, const char* filename, std::ofstream* layoutFile, CCM2Reader* pCCM, std::vector<WCHAR> charList, WCHAR* charIdx, const char* fontname, int size, bool is_bold, bool is_italic)
+{
+    HDC memDC = CreateCompatibleDC(nullptr);
+    SetBkMode(memDC, TRANSPARENT);
+
+    BITMAPINFO bmi = { 0 };
+    bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmi.bmiHeader.biWidth = TEXTURE_SIZE;
+    bmi.bmiHeader.biHeight = TEXTURE_SIZE;
+    bmi.bmiHeader.biPlanes = 1;
+    bmi.bmiHeader.biBitCount = 32;
+    bmi.bmiHeader.biCompression = BI_RGB;
+
+    void* pBits;
+    HBITMAP hBitmap = CreateDIBSection(memDC, &bmi, DIB_RGB_COLORS, &pBits, nullptr, 0);
+    if (!hBitmap) {
+        printf_s("Error creating DIB section\n");
+        DeleteDC(memDC);
+        return false;
+    }
+
+    SelectObject(memDC, hBitmap);
+
+    int nDPI = GetDeviceCaps(memDC, LOGPIXELSY);
+
+    int nHeight = -MulDiv(size, nDPI, 72);
+    int cWeight = FW_NORMAL;
+
+    if (is_bold)
+        cWeight = FW_BOLD;
+
+    HFONT hFont = CreateFontW(nHeight, 0, 0, 0, cWeight, is_italic, 0, 0, 1, 0, 0, 2, 2, LPCWSTR(fontname));
+    SelectObject(memDC, hFont);
+
+    int x = 1, y = 1;
+
+    while (*charIdx < charList.size())
+    {
+        WCHAR unicodeChar = charList[*charIdx];
+
+        WORD glyphIndex;
+        if (!GetGlyphIndicesW(memDC, &unicodeChar, 1, &glyphIndex, GGI_MARK_NONEXISTING_GLYPHS))
+        {
+            *charIdx += 1;
+            continue;
+        }
+
+        SIZE size;
+        GetTextExtentPoint32W(memDC, &unicodeChar, 1, &size);
+
+        if (size.cx == 0 || size.cy == 0)
+            continue;
+
+        if (x + size.cx > TEXTURE_SIZE)
+        {
+            x = 1;
+            y += size.cy;
+        }
+
+        if (y + size.cy > TEXTURE_SIZE)
+        {
+            *charIdx += 1;
+            *textureIdx += 1;
+            break;
+        }
+
+        SetTextColor(memDC, RGB(255, 255, 255));
+        RECT rect = { x, y, x + size.cx, y + size.cy }; // Adjust as needed
+        DrawTextW(memDC, &unicodeChar, -1, &rect, DT_LEFT | DT_TOP);
+
+        pCCM->AddTexRegion(TexRegion(x, y, x + size.cx, y + size.cy));
+
+        int prespace = 0;
+        int width = size.cx;
+        int advance = width + KERNING;
+
+        pCCM->AddGlyph(Glyph(unicodeChar, pCCM->GetGlyphCount(), *textureIdx, prespace, width, advance));
+
+        char buf[500];
+        sprintf_s(buf, "code=%d, textureId=%d, prespace=%d, width=%d, advance=%d, top=(%d, %d), bottom=(%d, %d)\n", unicodeChar, *textureIdx, prespace, width, advance, x, y, x + size.cx, y + size.cy);
+
+        std::string glyph_layout(buf);
+        layoutFile->write(glyph_layout.c_str(), glyph_layout.length());
+
+        x += size.cx;
+
+        *charIdx += 1;
+    }
+
+    BITMAP bmp;
+    GetObject(hBitmap, sizeof(BITMAP), &bmp);
+
+    BITMAPFILEHEADER bmfHeader = { 0 };
+    bmfHeader.bfType = 0x4D42; // "BM"
+    bmfHeader.bfSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + bmp.bmWidthBytes * bmp.bmHeight;
+    bmfHeader.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+
+    BITMAPINFOHEADER biHeader = { 0 };
+    biHeader.biSize = sizeof(BITMAPINFOHEADER);
+    biHeader.biWidth = bmp.bmWidth;
+    biHeader.biHeight = bmp.bmHeight;
+    biHeader.biPlanes = 1;
+    biHeader.biBitCount = 32;
+    biHeader.biCompression = BI_RGB;
+
+    std::ofstream file(filename, std::ios::out | std::ios::binary);
+    if (!file) {
+        printf_s("Error creating file\n");
+        file.close();
+        DeleteObject(hBitmap);
+        DeleteDC(memDC);
+        return false;
+    }
+
+    file.write(reinterpret_cast<const char*>(&bmfHeader), sizeof(BITMAPFILEHEADER));
+    file.write(reinterpret_cast<const char*>(&biHeader), sizeof(BITMAPINFOHEADER));
+    file.write(reinterpret_cast<const char*>(pBits), bmp.bmWidthBytes * bmp.bmHeight);
+
+    file.close();
+
+    // Cleanup
+    DeleteObject(hBitmap);
+    DeleteDC(memDC);
+
+    return true;
+}
+
 bool WriteGlyphsToBitmap(int* textureIdx, const char* filename, std::ofstream* layoutFile, CCM2Reader* pCCM, WCHAR* startChar, WCHAR* endChar, const char* fontname, int size, bool is_bold, bool is_italic)
 {
     HDC memDC = CreateCompatibleDC(nullptr);
@@ -253,13 +380,35 @@ int main(int argc, char* argv[])
 
     CCM2Reader ccm2;
 
-    if (!GenerateBitmapSubset(32, 65510, &textureId, filename.c_str(), &layout_file, &ccm2, fontname.c_str(), size, bold, italic))
-        return 0;
+    std::vector<WCHAR> charList;
+    std::ifstream inputFile("charlist.txt");
+    int num;
+
+    while (inputFile >> num)
+    {
+        printf_s("%d\n", num);
+        charList.push_back(num);
+    }
+
+    inputFile.close();
+
+    WCHAR start = 0;
+
+    while (start < charList.size())
+    {
+        char tex_name[255];
+        sprintf_s(tex_name, "%s_%04d.bmp", filename.c_str(), textureId);
+
+        printf_s("Write file %s\n", tex_name);
+
+        if (!WriteGlyphsToBitmapCharList(&textureId, std::string("Out/" + std::string(filename) + "/" + std::string(tex_name)).c_str(), &layout_file, &ccm2, charList, &start, fontname.c_str(), size, bold, italic))
+            return false;
+    }
 
     printf_s("Generating CCM2 file %s\n", ccm_name);
     ccm2.CreateCCM2(ccm_out, size, TEXTURE_SIZE, textureId);
     
-    printf_s("Generated %d textures\n", textureId);
+    printf_s("Generated %d textures\n", textureId + 1);
 
     return 1;
 }
