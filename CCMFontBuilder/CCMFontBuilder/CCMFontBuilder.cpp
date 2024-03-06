fa924aa5c5cf6b882a07e1c917d72de8307a8c28
Radai <lordradai@null.net>
2024-03-06 01:19:03 +0100

    fix

--- CCMFontBuilder/CCMFontBuilder/CCMFontBuilder.cpp
+++ CCMFontBuilder/CCMFontBuilder/CCMFontBuilder.cpp
@@ -5,6 +5,7 @@
 #include <filesystem>
 #include <DirectXTex.h>
 
+#include "StringHelper/StringHelper.h"
 #include "DebugOutput/DebugOutput.h"
 #include "FromsoftFormat/CCM2Reader/CCM2Reader.h"
 
@@ -49,18 +50,152 @@ bool FilterGlyph(WCHAR chr)
     return false;
 }
 
+void WriteGlyphsToDDS(HDC hdc, int* textureIdx, const char* filename, std::ofstream* layoutFile, CCM2Reader* pCCM, WCHAR* startChar, WCHAR* endChar, const char* fontname, int size, bool is_bold, bool is_italic)
+{
+    // Create Direct3D device
+    ID3D11Device* pDevice = nullptr;
+    ID3D11DeviceContext* pContext = nullptr;
+    D3D_FEATURE_LEVEL featureLevel;
+    HRESULT hr = D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_NULL, nullptr, 0, nullptr, 0, D3D11_SDK_VERSION, &pDevice, &featureLevel, &pContext);
+    if (FAILED(hr)) {
+        std::cerr << "Failed to create Direct3D device: " << std::hex << hr << std::endl;
+        return;
+    }
+
+    // Create texture
+    D3D11_TEXTURE2D_DESC descTex = {};
+    descTex.Width = TEXTURE_SIZE;
+    descTex.Height = TEXTURE_SIZE;
+    descTex.MipLevels = 1;
+    descTex.ArraySize = 1;
+    descTex.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+    descTex.SampleDesc.Count = 1;
+    descTex.Usage = D3D11_USAGE_DEFAULT;
+    descTex.BindFlags = D3D11_BIND_RENDER_TARGET;
+
+    ID3D11Texture2D* pTexture = nullptr;
+    hr = pDevice->CreateTexture2D(&descTex, nullptr, &pTexture);
+    if (FAILED(hr)) {
+        std::cerr << "Failed to create texture: " << std::hex << hr << std::endl;
+        pDevice->Release();
+        return;
+    }
+
+    // Create render target view
+    ID3D11RenderTargetView* pRTView = nullptr;
+    D3D11_RENDER_TARGET_VIEW_DESC descRTV = {};
+    descRTV.Format = descTex.Format;
+    descRTV.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+    hr = pDevice->CreateRenderTargetView(pTexture, &descRTV, &pRTView);
+    if (FAILED(hr)) {
+        std::cerr << "Failed to create render target view: " << std::hex << hr << std::endl;
+        pTexture->Release();
+        pDevice->Release();
+        return;
+    }
+
+    // Set render target
+    pContext->OMSetRenderTargets(1, &pRTView, nullptr);
+
+    // Clear texture to transparent
+    FLOAT clearColor[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
+    pContext->ClearRenderTargetView(pRTView, clearColor);
+
+    HDC memDC = CreateCompatibleDC(hdc);
+
+    SelectObject(memDC, pTexture);
+
+    int nDPI = GetDeviceCaps(hdc, LOGPIXELSY);
+
+    int nHeight = -MulDiv(size, nDPI, 72);
+    int cWeight = FW_NORMAL;
+
+    if (is_bold)
+        cWeight = FW_BOLD;
+
+    HFONT hFont = CreateFontW(nHeight, 0, 0, 0, cWeight, is_italic, 0, 0, 1, 0, 0, 2, 2, LPCWSTR(fontname));
+    SelectObject(memDC, hFont);
+
+    int x = 1, y = 1;
+    WCHAR unicodeChar = 0;
+    for (unicodeChar = *startChar; unicodeChar < *endChar; unicodeChar++)
+    {
+        WORD glyphIndex;
+        if (!GetGlyphIndicesW(hdc, &unicodeChar, 1, &glyphIndex, GGI_MARK_NONEXISTING_GLYPHS))
+            continue;
+
+        if (FilterGlyph(unicodeChar))
+            continue;
+
+        SIZE size;
+        GetTextExtentPoint32W(memDC, &unicodeChar, 1, &size);
+
+        if (size.cx == 0 || size.cy == 0)
+            continue;
+
+        if (x + size.cx > TEXTURE_SIZE)
+        {
+            x = 1;
+            y += size.cy;
+        }
+
+        if (y + size.cy > TEXTURE_SIZE)
+        {
+            *startChar = unicodeChar;
+            *textureIdx += 1;
+            break;
+        }
+
+        SetTextColor(memDC, RGB(255, 255, 255));
+        RECT rect = { x, y, x + size.cx, y + size.cy }; // Adjust as needed
+        DrawTextW(memDC, &unicodeChar, -1, &rect, DT_LEFT | DT_TOP);
+
+        pCCM->m_texRegions.push_back(TexRegion(x, y, x + size.cx, y + size.cy));
+
+        int prespace = 0;
+        int width = size.cx;
+        int advance = width + KERNING;
+
+        pCCM->m_glyphs.push_back(Glyph(unicodeChar, pCCM->m_glyphs.size(), *textureIdx, prespace, width, advance));
+
+        char buf[500];
+        sprintf_s(buf, "code=%d, textureId=%d, prespace=%d, width=%d, advance=%d, top=(%d, %d), bottom=(%d, %d)\n", unicodeChar, *textureIdx, prespace, width, advance, x, y, x + size.cx, y + size.cy);
+
+        std::string glyph_layout(buf);
+        layoutFile->write(glyph_layout.c_str(), glyph_layout.length());
+
+        x += size.cx;
+    }
+
+    *startChar = unicodeChar;
+
+    // Export texture to DDS
+    DirectX::ScratchImage image;
+    DirectX::CaptureTexture(pDevice, pContext, pTexture, image);
+    hr = DirectX::SaveToDDSFile(image.GetImages(), image.GetImageCount(), image.GetMetadata(), DirectX::DDS_FLAGS_NONE, StringHelper::ToWide(filename).c_str());
+    if (FAILED(hr)) {
+        std::cerr << "Failed to save texture to DDS file: " << std::hex << hr << std::endl;
+    }
+
+    // Cleanup
+    DeleteDC(memDC);
+    pRTView->Release();
+    pTexture->Release();
+    pDevice->Release();
+}
+
 void WriteGlyphsToBitmap(HDC hdc, int* textureIdx, const char* filename, std::ofstream* layoutFile, CCM2Reader* pCCM, WCHAR* startChar, WCHAR* endChar, const char* fontname, int size, bool is_bold, bool is_italic)
 {
+    HDC memDC = CreateCompatibleDC(hdc);
+    SetBkMode(memDC, TRANSPARENT);
+
     BITMAPINFO bmi = { 0 };
     bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
     bmi.bmiHeader.biWidth = TEXTURE_SIZE;
     bmi.bmiHeader.biHeight = TEXTURE_SIZE;
     bmi.bmiHeader.biPlanes = 1;
-    bmi.bmiHeader.biBitCount = 24;
+    bmi.bmiHeader.biBitCount = 32;
     bmi.bmiHeader.biCompression = BI_RGB;
-
-    HDC memDC = CreateCompatibleDC(hdc);
-    SetBkMode(memDC, TRANSPARENT);
 
     void* pBits;
     HBITMAP hBitmap = CreateDIBSection(memDC, &bmi, DIB_RGB_COLORS, &pBits, nullptr, 0);
@@ -70,7 +205,7 @@ void WriteGlyphsToBitmap(HDC hdc, int* textureIdx, const char* filename, std::of
         return;
     }
 
-    HBITMAP hOldBitmap = (HBITMAP)SelectObject(memDC, hBitmap);
+    SelectObject(memDC, hBitmap);
 
     int nDPI = GetDeviceCaps(hdc, LOGPIXELSY);
 
@@ -149,30 +284,18 @@ void WriteGlyphsToBitmap(HDC hdc, int* textureIdx, const char* filename, std::of
     biHeader.biWidth = bmp.bmWidth;
     biHeader.biHeight = bmp.bmHeight;
     biHeader.biPlanes = 1;
-    biHeader.biBitCount = bmp.bmBitsPixel;
+    biHeader.biBitCount = 32;
     biHeader.biCompression = BI_RGB;
 
     std::ofstream file(filename, std::ios::out | std::ios::binary);
     if (!file) {
         std::cerr << "Error creating file." << std::endl;
-        SelectObject(memDC, hOldBitmap);
         DeleteObject(hBitmap);
         DeleteDC(memDC);
         return;
     }
 
-    DirectX::TexMetadata metadata;
-    DirectX::ScratchImage scratchImage;
-    DirectX::CaptureTextureFromHbitmap(GetDC(NULL), hBitmap, scratchImage);
-
-    file.write(reinterpret_cast<const char*>(&bmfHeader), sizeof(BITMAPFILEHEADER));
-    file.write(reinterpret_cast<const char*>(&biHeader), sizeof(BITMAPINFOHEADER));
-    file.write(reinterpret_cast<const char*>(pBits), bmp.bmWidthBytes * bmp.bmHeight);
-
     // Cleanup
-    file.close();
-
-    SelectObject(memDC, hOldBitmap);
     DeleteObject(hBitmap);
     DeleteDC(memDC);
 }
@@ -250,7 +373,7 @@ int main(int argc, char* argv[])
     while (start < end)
     {
         char tex_name[255];
-        sprintf_s(tex_name, "%s_%04d.bmp", filename.c_str(), textureId);
+        sprintf_s(tex_name, "%s_%04d.dds", filename.c_str(), textureId);
 
         Debug::DebuggerMessage(Debug::LVL_DEBUG, "Write file %ls (startChar=%d, endChar=%d)\n", tex_name, start, end);
 
