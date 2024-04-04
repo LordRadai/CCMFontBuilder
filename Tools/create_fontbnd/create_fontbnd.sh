fd98a7338fd0d821da6d3c053be80fceb2a18f22
Radai <lordradai@null.net>
2024-04-04 13:19:10 +0200

    fix

--- Tools/create_fontbnd/create_fontbnd.sh
+++ Tools/create_fontbnd/create_fontbnd.sh
@@ -2,31 +2,37 @@ source "config.txt"
 
 if [ ! -d "$magick" ]; then
 	echo "Specified magick folder does not exist ($magick)"
+	sleep 2
 	exit 1
 fi
 
 if [ ! -d "$nvcompress" ]; then
 	echo "Specified nvcompress folder does not exist ($nvcompress)"
+	sleep 2
 	exit 1
 fi
 
 if [ ! -d "$witchybnd" ]; then
 	echo "Specified WitchyBND folder does not exist ($witchybnd)"
+	sleep 2
 	exit 1
 fi
 
 scripts/convert_dds.sh
 if [ $? -eq 1 ]; then
+	sleep 2
     exit 1
 fi
 
 scripts/create_tpf.sh
 if [ $? -eq 1 ]; then
+	sleep 2
     exit 1
 fi
 
 scripts/create_bnd4.sh
 if [ $? -eq 1 ]; then
+	sleep 2
     exit 1
 fi
 
