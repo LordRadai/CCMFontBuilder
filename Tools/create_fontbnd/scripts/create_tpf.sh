10375d3410651b635d89667c34736007ac2d57e0
Radai <lordradai@null.net>
2025-05-14 12:49:12 +0200

    fix

--- Tools/create_fontbnd/scripts/create_tpf.sh
+++ Tools/create_fontbnd/scripts/create_tpf.sh
@@ -14,30 +14,36 @@ fi
 
 mkdir -p "$font_folder"
 
-echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>
-<tpf>
-  <filename>font.tpf</filename>
-  <compression>None</compression>
-  <encoding>0x02</encoding>
-  <flag2>0x03</flag2>
-  <platform>PC</platform>
-  <textures>" > "$font_folder/_witchy-tpf.xml"
-
-cp "$input_folder"/* "$font_folder"/
-
 for dds_file in "$input_folder"/*.dds; do
-	name=$(basename -- "$dds_file")
+	name=$(basename -- "$dds_file" .dds)
+	nameExt=$(basename -- "$dds_file")
+
+	tpf_folder="$font_folder/$name"
+
+	mkdir -p "$tpf_folder"
+
+	mv "$dds_file" "$tpf_folder"
 	
+	xml_header="<?xml version=\"1.0\" encoding=\"utf-8\"?>
+	<tpf>
+	  <filename>"$name".tpf</filename>
+	  <compression>None</compression>
+	  <encoding>0x02</encoding>
+	  <flag2>0x03</flag2>
+	  <platform>PC</platform>
+	  <textures>"
+
+	echo "$xml_header" > "$tpf_folder/_witchy-tpf.xml"
+  	
 	xml_content="	<texture>
 		<name>$name</name>
 		<format>1</format>
 		<flags1>0x00</flags1>
 	</texture>"
 
-	echo "$xml_content" >> "$font_folder/_witchy-tpf.xml"
+	echo "$xml_content" >> "$tpf_folder/_witchy-tpf.xml"
+	echo "  </textures>
+	</tpf>" >> "$tpf_folder/_witchy-tpf.xml"
+
+	"$witchybnd" "$tpf_folder"
 done
-
-echo "  </textures>
-</tpf>" >> "$font_folder/_witchy-tpf.xml"
-
-"$witchybnd" "$font_folder"
