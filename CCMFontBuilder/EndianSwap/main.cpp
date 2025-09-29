927fb4c506e7d57a2a97137e412931bea38ea49d
Radai <lordradai@null.net>
2025-09-29 18:15:08 +0200

    endian swap

---
+++ CCMFontBuilder/EndianSwap/main.cpp
@@ -0,0 +1,23 @@
+#include "RCore.h"
+
+RLog* g_log;
+
+int main(int argc, char** argv)
+{
+    g_log = new RLog(MsgLevel_Debug, "Out\\", "EndianSwap.log");
+
+    if (argc < 2)
+    {
+        g_log->debugMessage(MsgLevel_Error, "Usage: %s \"fontname\"\n", argv[0]);
+        delete g_log;
+        return 0;
+    }
+
+    const std::string fontname = argv[1];
+    
+	DLFontData::DLFontDataCCM2* fontData = DLFontData::DLFontDataCCM2::loadFile(LPCWSTR(fontname.c_str()));
+	fontData->save(LPCWSTR((fontname + "_swapped").c_str()));
+
+    delete g_log;
+    return 1;
+}
